7f0dd6fd7276464df4d485d5ec2788b5fabfec08
ew2886 <31673898+ew2886@users.noreply.github.com>
2019-10-26 01:12:46 -0400

    Setting up firebase

--- lunch-buddy/screens/HomeScreen.js
+++ lunch-buddy/screens/HomeScreen.js
@@ -40,7 +40,7 @@ export default function HomeScreen() {
           </View>
 
           <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
+            Ftest
           </Text>
         </View>
 
--- lunch-buddy/screens/LinksScreen.js
+++ lunch-buddy/screens/LinksScreen.js
@@ -1,27 +1,91 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+  TextInput,
+  AlertIOS
+} from 'react-native';
 
-export default function LinksScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
-}
+import { db } from '../src/config';
 
-LinksScreen.navigationOptions = {
-  title: 'Links',
+let addItem = item => {
+  db.ref('/items').push({
+    name: item
+  });
 };
 
+export default class LinksScreen extends Component{
+  state = {
+    name: ''
+  };
+
+  handleChange = e => {
+    this.setState({
+      name: e.nativeEvent.text
+    });
+  };
+  handleSubmit = () => {
+    addItem(this.state.name);
+    AlertIOS.alert('Item saved successfully');
+  };
+
+  render() {
+    return (
+      <View style={styles.main}>
+        <Text style={styles.title}>Add Item</Text>
+        <TextInput style={styles.itemInput} onChange={this.handleChange} />
+        <TouchableHighlight
+          style={styles.button}
+          underlayColor="white"
+          onPress={this.handleSubmit}
+        >
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    padding: 30,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#6565fc'
   },
+  title: {
+    marginBottom: 20,
+    fontSize: 25,
+    textAlign: 'center'
+  },
+  itemInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
 });
